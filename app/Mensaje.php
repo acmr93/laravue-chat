5a8779a809693ecdc8984f9256ede7a6d61abc9a
Angela <acmrbi@gmail.com>
2019-08-09 18:04:35 -0430

    models and migrations (cosas, mensajes)

--- app/Mensaje.php
+++ app/Mensaje.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Mensaje extends Model
 {
-    //
+    protected $table = 'mensajes';
+
+	protected $fillable = ['user_id', 'valor'];
+
+	public function user()
+	{
+	    return $this->belongsTo(User::class, 'user_id');
+	}
 }
--- database/migrations/2019_08_09_172625_create_mensajes_table.php
+++ database/migrations/2019_08_09_172625_create_mensajes_table.php
@@ -14,7 +14,10 @@ class CreateMensajesTable extends Migration
     public function up()
     {
         Schema::create('mensajes', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('valor')->nullable();
             $table->timestamps();
         });
     }
